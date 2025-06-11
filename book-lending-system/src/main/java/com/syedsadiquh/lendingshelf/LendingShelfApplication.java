fd8a0700697ed7a376868b218effd2f6bd584aa6
syedsadiquh <syedsadiquh@gmail.com>
2025-06-11 13:03:41 +0530

    initial commit

---
+++ book-lending-system/src/main/java/com/syedsadiquh/lendingshelf/LendingShelfApplication.java
@@ -0,0 +1,13 @@
+package com.syedsadiquh.lendingshelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingShelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LendingShelfApplication.class, args);
+	}
+
+}
---
+++ book-lending-system/src/test/java/com/syedsadiquh/lendingshelf/LendingShelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syedsadiquh.lendingshelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingShelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
