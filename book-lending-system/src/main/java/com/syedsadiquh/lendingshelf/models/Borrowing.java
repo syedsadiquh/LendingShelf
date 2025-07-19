cb33f9c7f269eb2d7780faff735a4243f2a687b3
syedsadiquh <syedsadiquh@gmail.com>
2025-07-20 00:24:02 +0530

    style(imports): removed unused imports \n Refactoring

--- book-lending-system/src/main/java/com/syedsadiquh/lendingshelf/models/Borrowing.java
+++ book-lending-system/src/main/java/com/syedsadiquh/lendingshelf/models/Borrowing.java
@@ -1,6 +1,5 @@
 package com.syedsadiquh.lendingshelf.models;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
